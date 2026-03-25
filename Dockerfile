2e8c17ecc2e9533ae8c0d8e913ae888f907ce4b6
DjibrilMTRAORE <traodjibril.250@gmail.com>
2026-03-25 14:43:36 +0000

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
-FROM php:8.3-fpm
-RUN docker-php-ext-install pdo_mysql
+FROM php:8.2-fpm
+
+WORKDIR /var/www/html
+
+COPY . .
+
+RUN apt-get update && apt-get install -y libpq-dev \
+    && docker-php-ext-install pdo_pgsql
--- core/Database.php
+++ core/Database.php
@@ -1,10 +1,10 @@
 <?php
 class Database {
-    private $host = "localhost";
+    private $host = "postgres_db";
     private $user = "postgres";
     private $password = "sonbeckscoding"; 
     private $database = "postgres";
-    private $port = 5434;
+    private $port = 5432; 
     public $conn;
 
     public function __construct() {
@@ -15,6 +15,7 @@ class Database {
             
             $this->conn = new PDO($dsn, $this->user, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         } catch (PDOException $e) {
             die("Connexion échouée : " . $e->getMessage());
         }
